6afee245c25a462a9cb4d2851b47b4ef5f7b256e
dias <giyos.uzb@gmail.com>
2023-10-04 23:18:33 +0500

    first work

---
+++ js/script.js
@@ -0,0 +1,36 @@
+'use strict'
+
+// alert('Hello');
+// const result = confirm('Are you here?')
+// console.log(result)
+
+
+// const answer = prompt('Вам есть 18?', '')
+// console.log(typeof (answer))
+
+
+// const answer = []
+// answer[0] = prompt('Как ваше имя?', '')
+// answer[1] = prompt('Как ваше фамилия?', '')
+// answer[2] = prompt('Сколько вам лет?', '')
+// console.log(typeof (answer))
+
+
+// const category = 'toys'
+// console.log(`https://someurl.com/${category}/5`)
+// const user = 'dias'
+// alert(`Привет, ${user}`)
+
+
+// console.log('arr' + ' - object')
+// console.log(4 + ' - object')
+
+
+// let incr = 10,
+//   decr = 10
+// ++incr
+// --decr
+// console.log(++incr)
+// console.log(--decr)
+
+
