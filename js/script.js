874e1795a1c8053a5c0402d0545c3b9c961c4f56
dias <giyos.uzb@gmail.com>
2023-10-06 21:10:08 +0500

    add to changes

--- js/script.js
+++ js/script.js
@@ -1,29 +1,14 @@
 "use strict";
 
-// alert('Hello');
-// const result = confirm('Are you here?')
-// console.log(result)
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-// const answer = prompt('Вам есть 18?', '')
-// console.log(typeof (answer))
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
-// const answer = []
-// answer[0] = prompt('Как ваше имя?', '')
-// answer[1] = prompt('Как ваше фамилия?', '')
-// answer[2] = prompt('Сколько вам лет?', '')
-// console.log(typeof (answer))
+const a = prompt("Один из просмотренных фильмов?", "");
 
-// const category = 'toys'
-// console.log(`https://someurl.com/${category}/5`)
-// const user = 'dias'
-// alert(`Привет, ${user}`)
-
-// console.log('arr' + ' - object')
-// console.log(4 + ' - object')
-
-let incr = 10,
-  decr = 10;
-++incr;
---decr;
-console.log(++incr);
-console.log(--decr);
